cf7b1fe5751379dd557eaf31410cbbc8ab636be3
Aiden Taylor <aiden.taylor@protonmail.com>
2023-03-07 22:06:54 -0700

    Added functionality with ``ordgem()``.

--- src/tarottool/gemwrap.rs
+++ src/tarottool/gemwrap.rs
@@ -1,5 +1,6 @@
 use crate::tarottool::gemcalc::asciigem;
 use crate::tarottool::gemcalc::enggem;
+use crate::tarottool::gemcalc::ordgem;
 
 // This function wraps the behaviour of converting the input to Gematria.
 pub fn gematria(input: &String, gemtype: usize) -> usize {
@@ -12,13 +13,19 @@ pub fn gematria(input: &String, gemtype: usize) -> usize {
                 value = value + enggem(chr);
             }
         }
-
+        // For Ascii Gematria
         1 => {
             for chr in input.chars() {
                 value = value + asciigem(chr);
             }
         }
-
+        // For Ordinal Gematria
+        2 => {
+            for chr in input.chars() {
+                value = value + ordgem(chr);
+            }
+        }
+        // Otherwise, soil bed.
         _ => panic!("Error! Value for Gematria type not valid!"),
     }
 
