40ea6793042c02940a7e1ff82ffc3885dd3b192b
Aiden Taylor <aiden.taylor@protonmail.com>
2023-03-07 22:07:27 -0700

    Altered to work with the new argument-parsing methods.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 pub mod tarottool;
 use crate::tarottool::gemwrap::gematria;
 
+pub mod argparse;
+use crate::argparse::argumentparser;
+use crate::argparse::helpprint;
+
 use std::env;
+
+// Used as the number of arguments.
+const NUMBER_OF_OPTIONS: usize = 1;
 
 fn main() {
     // Initializing the program:
-    // 0: We see if the input of the program is null.
+    // We see if the input of the program is null.
     // If so, we print a help message, and terminate.
     let mut argbuff = env::args().nth(1);
     if argbuff == None {
@@ -13,77 +20,57 @@ fn main() {
         return;
     }
 
-    // 1: We call variables relating to initialization.
-    let mut input: String; // Used as a readable buffer for input.
-    let mut query: String = "0".to_string(); // Used as a storage for the actual query.
-    let mut gematriatype: usize = 0; // Relates to the type of Gematria.
-
-    // 2: We parse the arguments with a loop until we find the end of the arguments:
-    let mut argumenthandler: usize = 1; // This is used to see which argument we're on.
-
-    loop {
-        // We take the argument...
-        argbuff = env::args().nth(argumenthandler);
-        // Then we read to see if it's data is null. If so, we break this loop.
-        if argbuff == None {
-            break;
-        }
-
-        // We convert the argument into a string for reading.
-        input = argbuff.expect("Failed to read string.").to_string();
-
-        // If we are to be printing the help page...
-        if input == "-H" {
-            helpprint();
-            return;
-        }
-
-        // If we are to be changing the Gematria with -g...
-        if input == "-g" {
-            // We then need to parse the next argument.
-            // We add one to argumenthandler to prevent future errors.
-            argumenthandler += 1;
-            let secondargbuff = env::args().nth(argumenthandler);
-            // In the case of a null value, we print an error, and terminate
-            // the process of parsing this command.
-            if secondargbuff == None {
-                println!("Error! Option '-g' has no following number!");
-                println!("Gematria will be set to 0 (Default). Run with -H for help.");
-                break;
-            }
-            // Then we process the second argument into a string for the sake of
-            // comparison.
-            let secondarg: String = secondargbuff.expect("Failed to read string.").to_string();
-            // Then we see what to do with it:
-            match secondarg.as_str() {
-		"0" => gematriatype = 0,
-                "1" => gematriatype = 1,
-		_ => println!("Error! Option '-g' has an invalid number!\nGematria will be set to 0 (Default). Run with -H for help."),
-	    }
-        }
-        // Otherwise, we assume that the input given is our query; and we write that
-        // to the dedicated query value.
-        else {
-            query = input.to_string();
-        }
-
-        argumenthandler += 1;
+    // We also check to see if the -H option was passed.
+    // we do this first, as running the -H option means that the program
+    // should terminate.
+    let input = argbuff
+        .expect("Failed to parse the first argument given.")
+        .to_string();
+    if input == "-H" {
+        helpprint();
+        return;
     }
 
-    let value: usize = gematria(&query.to_string(), gematriatype);
+    // Otherwise, we write to a vector all of the passed arguments from 1
+    // until the end of the argument list.
+    let mut arguments: Vec<String> = vec![String::from("0"); env::args().len()];
+    // And a vector for the resulting values of our arguments; called "options".
+    let mut options: Vec<usize> = vec![0; NUMBER_OF_OPTIONS];
+    /*
+     * GUIDE:
+     *   Value 0 - Gematria (Should be 0, 1, or 2.)
+     */
 
-    println!("");
-    println!(
-        "Query:		{query}
-            \nGematria:	{gematriatype}
-            \nValue:		{value}"
-    );
-}
+    // Now we finally fill the arguments vector.
+    for i in 1..env::args().len() {
+        argbuff = env::args().nth(i);
+        arguments[i] = argbuff
+            .expect("Failed to parse argument {i} when writing!")
+            .to_string();
+    }
 
-pub fn helpprint() {
-    println!("HELP PAGE FOR DIVRS:\n");
-    println!("Format:\n ./divrs ''<query>'' <arguments>\n");
-    println!("Currently supported arguments:");
-    println!("-H          -  Prints this message.");
-    println!("-g <0 - 1>  -  Changes the type of Gematria used.\n0 = English\n1 = ASCII");
+    // Wi2th that being done, we can now run the in-effect macro to take the arguments,
+    // and write them to our options:
+    let querynum: usize = argumentparser(arguments, &mut options);
+    // The function also returns the number that is the argument in which the query
+    // is stored, so we write that to query.
+    argbuff = env::args().nth(querynum);
+    let query = argbuff
+        .expect("Failed to parse argument {querynum} when writing to query!")
+        .to_string();
+
+    // Finally, we get to convert our query into a Gematria value.
+    let value = gematria(&query, options[0]);
+
+    println!("\n");
+    println!("QUERY:    {}", query);
+    println!("VALUE:    {}", value);
+    println!("GEMATRIA TYPE:");
+
+    match options[0] {
+        0 => println!("English"),
+        1 => println!("ASCII"),
+        2 => println!("Ordinal"),
+        _ => panic!("What in the goddamn?"),
+    };
 }
